e4ae03d4f4543287d8ce8489cd9b56bef7c1e4c3
TESVLE <45486900+TESVLE@users.noreply.github.com>
2021-02-02 22:35:16 +0800

    Create Form.php

---
+++ Form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8" />
+ 	<title>Commissioner Account Creation</title>
+ 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ </head>
+ <body class="blurBg-true" style="background-color:#EBEBEB">
+
+
+
+ <link rel="stylesheet" href="comreg_files/formoid1/formoid-biz-red.css" type="text/css" />
+ <script type="text/javascript" src="comreg_files/formoid1/jquery.min.js"></script>
+ <form class="formoid-biz-red" style="background-color:#1A2223;font-size:14px;font-family:'Open Sans','Helvetica Neue', 'Helvetica', Arial, Verdana, sans-serif;color:#ECECEC;max-width:480px;min-width:150px" method="get" action="test.php"><div class="title"><h2>Commissioner Account Creation</h2></div>
+ 	<div class="element-input"><label class="title"><span class="required">*</span></label><input class="large" type="text" name="username" required="required" placeholder="Username"/></div>
+ 	<div class="element-email"><label class="title"><span class="required">*</span></label><input class="large" type="email" name="email" value="" required="required" placeholder="Email"/></div>
+ 	<div class="element-password"><label class="title"><span class="required">*</span></label><input class="large" type="password" name="password" value="" required="required" placeholder="Password"/></div>
+ 	<div class="element-password"><label class="title"><span class="required">*</span></label><input class="large" type="password" name="password1" value="" required="required" placeholder="Confirm Password"/></div>
+ 	<div class="element-name"><label class="title"><span class="required">*</span></label><span class="nameFirst"><input placeholder="Name First" type="text" size="8" name="fname" required="required"/></span><span class="nameLast"><input placeholder="Name Last" type="text" size="14" name="lname" required="required"/></span></div>
+ 	<div class="element-radio"><label class="title">I Commissioned Marck for:<span class="required">*</span></label><div class="column column1"><label><input type="radio" name="radio" value="Digital Illustration" required="required"/><span>Digital Illustration</span></label><label><input type="radio" name="radio" value="Website Building" required="required"/><span>Website Building</span></label><label><input type="radio" name="radio" value="Brokership & Site Admin" required="required"/><span>Brokership & Site Admin</span></label></div><span class="clearfix"></span>
+ </div>
+ 	<div class="element-separator"><hr><h3 class="section-break-title"></h3></div>
+ <div class="submit"><input type="submit" name"submit" value="Submit & Login"/></div></form><p class="frmd"><script type="text/javascript" src="comreg_files/formoid1/formoid-biz-red.js"></script>
+
+
+
+ </body>
+ </html>
